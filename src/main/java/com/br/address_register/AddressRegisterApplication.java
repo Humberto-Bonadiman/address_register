5925e06fcba454b2e76c3070906986e6c5f08197
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2023-01-20 15:38:04 -0400

    build: initial configuration

---
+++ src/main/java/com/br/address_register/AddressRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.br.address_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/address_register/AddressRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.address_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
