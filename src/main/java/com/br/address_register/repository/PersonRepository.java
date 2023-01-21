69af87f55434dfd08b44c8f11c81b350006cace3
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2023-01-20 21:44:01 -0400

    feat: create PersonRepository and AddressRepository

---
+++ src/main/java/com/br/address_register/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.br.address_register.repository;
+
+import com.br.address_register.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
